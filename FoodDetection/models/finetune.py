83043cd990297346223580ab5568ce8c330e3ab9
ngwallace <narek2106@gmail.com>
2019-05-13 21:51:08 +0400

    check

--- FoodDetection/models/finetune.py
+++ FoodDetection/models/finetune.py
@@ -1,1 +1,12 @@
 from keras.applications import InceptionResNetV2
+
+
+def main():
+  inception_model = InceptionResNetV2()
+
+  for layer in inception_model.layers:
+    print(layer.name)
+    layer.trainable = False
+
+if __name__ == '__main__':
+  main()
